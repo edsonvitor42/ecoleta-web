90bf70398bd14610b7fff9a88832961a1bc05e21
Edson Vitor <edson@taqe.com.br>
2020-06-07 19:58:17 -0300

    Finalizando o projeto!

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
+# Ecoleta Web
+Ecoleta é um projeto da [NVW](https://nextlevelweek.com/) entre os dias 01/06/2020 até 07/06/2020, sobre uma aplicação que interliga o usuário com instituições de coleta de materiais recicláveis.
+
+## Telas da aplicação:
+Tela inicial da aplicação:
+<img src="./tutorial/screens/home.png" width="100%" height="500px">
+
+## Como executar o projeto:
+Utilize o node na versão 12.16.3
+
+Efetue o clone do projeto:
+```bash
+git clone https://github.com/Architect42/ecoleta-web.git
+```
+
+Acesse a pasta do projeto e atualize as dependências do projeto com o comando:
+```bash
+npm install
+```
+
+Comando para rodar o projeto:
+```bash
+npm run dev
+```
+
 ## Extensões:
 [React Developer Tools](https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi/related)
 
@@ -5,3 +30,6 @@
 Para a área de mapas foi utilizado uma lib publica de mapas chamada [leaflet](https://leafletjs.com/).
 
 Para a lista de cidades e UF's foi utilizado a api de localidades do [IBGE](https://servicodados.ibge.gov.br/api/docs/localidades?versao=1).
+
+Yup responsável por válidar os campos.
+[yup](https://www.npmjs.com/package/yup)
--- src/pages/Point/Create/index.tsx
+++ src/pages/Point/Create/index.tsx
@@ -5,6 +5,7 @@ import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
 import api from '../../../services/api';
 import axios from 'axios';
+import Dropzone from '../../../components/dropzone';
 
 import './styles.css';
 
@@ -44,6 +45,7 @@ const CreatePoint = () => {
     const [selectedUf, setSelectedUf] = useState('0');
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const history = useHistory();
 
@@ -127,20 +129,24 @@ const CreatePoint = () => {
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
-        };
+        const data = new FormData()
+
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+
+        if (selectedFile) {
+            data.append('image', selectedFile);
+        }
 
         await api.post('points', data);
 
-        alert(`Ponto de coleta ${data.name} cadastrado com sucesso!`);
+        alert(`Ponto de coleta ${name} cadastrado com sucesso!`);
 
         history.push('/');
     }
@@ -157,6 +163,9 @@ const CreatePoint = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br /> ponto de coleta</h1>
+
+                <Dropzone onFileUploaded={setSelectedFile} />
+
                 <fieldset>
                     <legend>
                         <h2>Dados</h2>
